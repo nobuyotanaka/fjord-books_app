9fb59c3584d82f4059fa186b0a89fbdf02888cde
nobuyotanaka <kshiris1028@gmail.com>
2022-06-29 17:58:15 +0900

    郵便番号、住所、自己紹介のカラムをユーザーテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_023742) do
+ActiveRecord::Schema.define(version: 2022_06_29_085340) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2022_06_27_023742) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "zipcode"
+    t.string "address"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
