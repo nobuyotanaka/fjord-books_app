3d4fec8c25e0ddcdce728ee63041ea63692a8cb2
nobuyotanaka <kshiris1028@gmail.com>
2022-07-03 17:08:36 +0900

    ユーザー詳細ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+
+
+
+
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).per(2)
   end
 
   def show
+    @user = User.find(params[:id])  
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'books#index'
-  get 'users/index'
-  get 'users/show'
   devise_for :users
+  resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
