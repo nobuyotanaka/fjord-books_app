1e594453013d64ad130949d1bde93274a7771b5b
nobuyotanaka <kshiris1028@gmail.com>
2022-07-04 10:56:44 +0900

    編集と新規登録でパラメータを反映

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,18 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters_signup, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+ #新規登録が可能なパラメータ
+ def configure_permitted_parameters_signup
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zipcode, :address, :introduction])
+end
+
+
+  #編集が可能なパラメータ
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zipcode, :address, :introduction])
+  end
 end
