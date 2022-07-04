897bebd202d20ddee3365162a737f75707e172b3
nobuyotanaka <kshiris1028@gmail.com>
2022-07-04 10:59:12 +0900

    privateからprotectに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters_signup, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
- #新規登録が可能なパラメータ
- def configure_permitted_parameters_signup
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zipcode, :address, :introduction])
-end
+  protected
+
+  #新規登録が可能なパラメータ
+  def configure_permitted_parameters_signup
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zipcode, :address, :introduction])
+  end
 
 
   #編集が可能なパラメータ
