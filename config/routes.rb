532d1467a3aa085932197a7056ad5abf64e02991
nobuyotanaka <kshiris1028@gmail.com>
2022-07-04 09:15:49 +0900

    必要なアクションだけをroutesに設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'books#index'
   devise_for :users
   resources :books
-  resources :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
