54adc2ab055bd7e607988ff0dd369aeae5d91dd2
nobuyotanaka <kshiris1028@gmail.com>
2022-08-15 15:25:43 +0900

    外部キーはusersテーブルを参照する

--- db/migrate/20220815061604_create_follow_relationships.rb
+++ db/migrate/20220815061604_create_follow_relationships.rb
@@ -3,7 +3,6 @@ class CreateFollowRelationships < ActiveRecord::Migration[6.1]
     create_table :follow_relationships do |t|
       t.references :following, null: false, foreign_key: { to_table: :users }
       t.references :follower, null: false, foreign_key: { to_table: :users }
-
       t.timestamps
     end
   end
