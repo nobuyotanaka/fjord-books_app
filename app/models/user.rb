367de47560b7cd7bace645ae558c726d10916a72
nobuyotanaka <kshiris1028@gmail.com>
2022-08-15 21:49:23 +0900

    has_many以下の名前を複数形に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
 #　relations
 #-----------------
 
-  has_many :following_relationship, class_name: 'FollowRelationship', foreign_key: 'following_id', dependent: :destroy
-  has_many :follower_relationship, class_name: 'FollowRelationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following_relationships, class_name: 'FollowRelationship', foreign_key: 'following_id', dependent: :destroy
+  has_many :follower_relationships, class_name: 'FollowRelationship', foreign_key: 'follower_id', dependent: :destroy
 
-  has_many :followings, through: :following_relationship, source: :follower
-  has_many :followers, through: :follower_relationship, source: :following
+  has_many :followings, through: :following_relationships, source: :follower
+  has_many :followers, through: :follower_relationships, source: :following
 end
