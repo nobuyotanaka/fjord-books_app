fa99bc3a0d068e9b7a6f31e87cbeda6e793c4142
nobuyotanaka <kshiris1028@gmail.com>
2022-08-16 20:59:19 +0900

    フォローするときの動作

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -1,2 +1,12 @@
 class FollowRelationshipsController < ApplicationController
+
+  def create
+    @user = User.find(params[:user_id])
+    current_user.follow(params[@user])
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    
+  end
 end
