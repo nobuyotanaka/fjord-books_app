93ab6e497294dee9d736bfe1d53a1ed8bc4d5b9b
nobuyotanaka <kshiris1028@gmail.com>
2022-08-18 22:15:47 +0900

    paramsの中身を訂正

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -2,12 +2,12 @@ class FollowRelationshipsController < ApplicationController
   before_action :set_user
 
   def create
-    current_user.follow(params[@user])
+    current_user.follow(params[:user_id])
     redirect_to user_path(@user)
   end
 
   def destroy
-    current_user.unfollow(params[@user])
+    current_user.unfollow(params[:user_id])
     redirect_to user_path(@user)
   end
 
