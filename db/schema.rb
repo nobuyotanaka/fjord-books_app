f1898b207ca0a7ccbbcca69aa88210ca37c58ab3
nobuyotanaka <kshiris1028@gmail.com>
2022-08-22 21:40:51 +0900

    reportsテーブルのカラム名commentをcontentsに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_21_013415) do
+ActiveRecord::Schema.define(version: 2022_08_22_123808) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2022_08_21_013415) do
 
   create_table "reports", force: :cascade do |t|
     t.string "title", null: false
-    t.text "comment", null: false
+    t.text "contents", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
