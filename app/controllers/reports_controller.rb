af11d0ac46fe166ad3c3a3726ce0f1c9a47972fa
nobuyotanaka <kshiris1028@gmail.com>
2022-08-29 11:38:22 +0900

    reportの詳細ページでコメントができる

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,71 +1,62 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[ show edit update destroy ]
 
-  # GET /reports or /reports.json
   def index
     @reports = Report.all.order(:id).page(params[:page])
   end
 
-  # GET /reports/1 or /reports/1.json
   def show
     @report = Report.find(params[:id])
+    @comment = @report.comments.build
   end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit
   end
 
-  # POST /reports or /reports.json
   def create
     @report = current_user.reports.new(report_params)
 
     respond_to do |format|
       if @report.save
-        format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
-        format.json { render :show, status: :created, location: @report }
+        redirect_to report_url(@report), notice: "Report was successfully created." 
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity 
       end
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
   def update
     respond_to do |format|
       if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
+        redirect_to report_url(@report), notice: "Report was successfully updated." 
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
 
     respond_to do |format|
-      format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
+      redirect_to reports_url, notice: "Report was successfully destroyed." 
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_report
       @report = Report.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def report_params
       params.require(:report).permit(:title, :contents)
+    end
+
+    def set_commentable
+      @commentable = Report.find(params[:report_id])
     end
 end
