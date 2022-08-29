c35044b17a0b5d939bdb9b609b5b962e44ccbe69
nobuyotanaka <kshiris1028@gmail.com>
2022-08-29 18:41:44 +0900

    不要な行を削除

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -4,7 +4,5 @@ class FollowRelationship < ApplicationRecord
   belongs_to :following, class_name: 'User'
   belongs_to :follower, class_name: 'User'
 
-  validates :following_id, presence: true
-  validates :follower_id, presence: true
   validates :following_id, uniqueness: { scope: :follower_id }
 end
