96341e4805e7e569a6926abe31a6bfc2e841510d
nobuyotanaka <kshiris1028@gmail.com>
2022-08-29 11:37:20 +0900

    routesにネストしたcommentを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  resources :comments
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   
-  resources :books
+  resources :books do
+    resources :comments, module: :books
+  end
 
-  resources :reports
+  resources :reports do
+    resources :comments, module: :reports
+  end
   
+  # resources :comments
+
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
